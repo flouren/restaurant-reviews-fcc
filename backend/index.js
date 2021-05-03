9402ff87b522ff986f531a6a0326718a714c31e4
flouren <bbaal@wp.pl>
2021-05-03 21:35:27 +0200

    serv fundamentals

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+import app from './server.js'
+import mongodb from 'mongodb'
+import dotenv from 'dotenv'
+import RestaurantsDAO from './dao/restaurantsDAO.js'
+
+dotenv.config()
+
+const MongoClient = mongodb.MongoClient
+
+const port = process.env.PORT || 8000
+
+MongoClient.connect(
+    process.env.RESTREVIEWS_DB_URI,{
+        useNewUrlParser:true,
+        useUnifiedTopology:true,
+        poolSize:50,
+        wtimeout:2500
+    }
+)
+.then( async client=>{
+    console.log('MongoDB connected ..')
+    await RestaurantsDAO.injectDB(client)
+    app.listen(port, console.log(`Listening on port ${port} ..`))
+})
+.catch(err=>{
+    console.error(err.stack,err.message)
+    process.exit(1)
+})
--- backend/server.js
+++ backend/server.js
@@ -7,5 +7,9 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.use('/api/v1/restaurants')
+app.use('/api/v1/restaurants',restaurants)
+app.use('*', (req,res)=>{
+    res.status(404).json({error:"Ooops! Page Not Found!"})
+})
 
+export default app
