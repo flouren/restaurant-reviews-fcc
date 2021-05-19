5b178707d3286b936aaad14bc3dee81a324cd98e
flouren <bbaal@wp.pl>
2021-05-19 22:53:16 +0200

    added useState  handling

--- frontend/src/components/restaurants-list.js
+++ frontend/src/components/restaurants-list.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
+import RestaurantDataService from "../services/restaurant"
+import {Link} from "react-router-dom"
 
+const RestaurantsList= props => {
 
-function RestaurantsList() {
+  const [restaurants,setRestaurants] = useState([])
+  const [searchName,setsearchName] = useState("")
+  const [searchZip,setSearchZip] = useState("")
+  const [searchCuisine,setSearchCuisine] = useState("")
+  const [cuisines,setCuisines] = useState(["All Cuisines"])
+  
   return (
     <div className="App">
-      
+      Restaurants List
     </div>
   );
 }
